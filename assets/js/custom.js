89fdfd0cffe5745ddf65138703c4f20693a3ccec
Jake Bonham <jake@docforce.io>
2019-08-16 16:48:58 -0700

    added new rfcs pages. Updated mobile menu

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,17 @@
 (function($){
 
+  // Mobile menu.  
+  $('[data-toggle="collapse"]').on('click', function() {
+    $navMenuCont = $($(this).data('target'));
+    $navMenuCont.toggleClass('show');
+    $(".menu-overlay").toggleClass('active');
+  });
+  $(".menu-overlay").click(function(event) {
+    $(".navbar-toggler").trigger("click");
+  });
+
+
+
   //Add active class to sidebar links
   function sidebarActiveClass() {
     var curentFile = window.location.pathname.split("/").pop();
